e308ece4b8f7d35c979570d98c7061adfb0b4ec8
PRASHANT TYAGI <prashant_tyagi@excellencetechnologies.in>
2018-10-10 16:36:18 +0530

    add code for get all categories and categories product for shopify

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -6,8 +6,25 @@ export class CategoryController extends BaseAPIController {
     /* Controller for get all categories*/
     getAllCategories = async (req, res, next) => {
         try {
+            let data = [];
             let manage_data = await CategoryProvider.setPathForGetCategories(req.headers, req.url_path, req.method, req.store);
             let getAllCategories = await request.requestToServer(manage_data);
+            if (req.isMagento) {
+                getAllCategories = getAllCategories;
+            } else if (req.isShopify) {
+                let allCategories = getAllCategories["shop"]["collections"]["edges"];
+                allCategories.forEach((item, key) => {
+                    item["node"]["name"] = item["node"]["title"];
+                    data.push(item["node"]);
+                })
+                getAllCategories = {
+                    "name": "Default Category",
+                    "is_active": true,
+                    "children_data": data
+                };
+            } else {
+                // coming soon
+            }
             this.handleSuccessResponse(res, next, getAllCategories)
         } catch (err) {
             this.handleErrorResponse(res, err)
@@ -17,17 +34,52 @@ export class CategoryController extends BaseAPIController {
     /* Controller for get product for a category*/
     categoryProduct = async (req, res, next) => {
         try {
+            let data = [];
+            let itemData = {};
             let manage_data = await CategoryProvider.setPathForGetCategoryProduct(req.body, req.headers, req.url_path, req.store);
             let categoryProduct = await request.requestToServer(manage_data);
-            if(req.isMagento){
+            if (req.isMagento) {
                 categoryProduct['items'].forEach((value, key) => {
                     var imageLink = value.custom_attributes.find((details) => {
-                        if(details.attribute_code == "image"){
+                        if (details.attribute_code == "image") {
                             return details.value;
                         }
                     })
                     categoryProduct.items[key].imageLink = imageLink.value;
                 })
+            } else if (req.isShopify) {
+                let allCategorieProduct = categoryProduct["node"]["products"]["edges"];
+                allCategorieProduct.forEach((item, key) => {
+                    let allVariants = item["node"]["variants"]["edges"];
+                    let variantData = [];
+                    allVariants.forEach((item, key) => {
+                        variantData.push(item["node"]);
+                    })
+                    itemData = {
+                        "id": item["node"]["id"],
+                        "sku": "",
+                        "name": item["node"]["title"],
+                        "attribute_set_id": null,
+                        "price": null,
+                        "status": item["node"]["availableForSale"],
+                        "visibility": null,
+                        "type_id": "",
+                        "created_at": item["node"]["createdAt"],
+                        "updated_at": item["node"]["updatedAt"],
+                        "product_links": [item["node"]["onlineStoreUrl"]],
+                        "tier_prices": [],
+                        "imageLink": item["node"]["images"]["edges"][0]["node"]["originalSrc"],
+                        "description": item["node"]["description"],
+                        "descriptionHtml": item["node"]["descriptionHtml"],
+                        "variants": variantData
+                    };
+                    data.push(itemData);
+                })
+                categoryProduct = {
+                    "items": data
+                }
+            } else {
+                // coming soon
             }
             this.handleSuccessResponse(res, next, categoryProduct)
         } catch (err) {
@@ -35,6 +87,6 @@ export class CategoryController extends BaseAPIController {
         }
     }
 }
-    
+
 const controller = new CategoryController()
 export default controller
--- src/providers/CategoryProvider.js
+++ src/providers/CategoryProvider.js
@@ -1,15 +1,36 @@
 import config from '../config';
 let setPathForGetCategories = (async function (headers, url_path, method, store) {
     let manage_data = {};
-    if(store == 'magento'){
+    if (store == 'magento') {
         manage_data.endUrl = url_path + "/V1/categories";
         manage_data.method = method;
         manage_data.app_id = headers.app_id;
         manage_data.authorization = headers.authorization;
         return manage_data;
-    } else if(store == 'shopify') {
-        manage_data.endUrl = url_path + "/custom_collections.json";
-        manage_data.method = method;
+    } else if (store == 'shopify') {
+        manage_data.body = `{
+            shop {
+              collections(first: 6) {
+                edges {
+                  node {
+                    id
+                    title
+                    handle
+                    description
+                    updatedAt
+                    descriptionHtml
+                  }
+                }
+                pageInfo {
+                  hasNextPage
+                }
+              }
+            }
+          }`;
+        manage_data.endUrl = url_path;
+        manage_data.method = "POST";
+        manage_data.contentType = "application/graphql";
+        manage_data.storefrontAccessToken = config.storefrontAccessToken;
         return manage_data;
     } else {
         // coming soon
@@ -17,15 +38,75 @@ let setPathForGetCategories = (async function (headers, url_path, method, store)
 });
 let setPathForGetCategoryProduct = (async function (body, headers, url_path, store) {
     let manage_data = {};
-    if(store == 'magento'){
-        manage_data.endUrl = url_path + "/V1/products?searchCriteria[filter_groups][0][filters][0][field]=category_id&searchCriteria[filter_groups][0][filters][0][value]="+body.category_id+"&searchCriteria[sortOrders][0][field]="+body.sortBy+"&searchCriteria[sortOrders][0][direction]="+body.sortOrder+"&searchCriteria[pageSize]="+body.pageSize+"&searchCriteria[currentPage]="+body.currentPage;
+    if (store == 'magento') {
+        manage_data.endUrl = url_path + "/V1/products?searchCriteria[filter_groups][0][filters][0][field]=category_id&searchCriteria[filter_groups][0][filters][0][value]=" + body.category_id + "&searchCriteria[sortOrders][0][field]=" + body.sortBy + "&searchCriteria[sortOrders][0][direction]=" + body.sortOrder + "&searchCriteria[pageSize]=" + body.pageSize + "&searchCriteria[currentPage]=" + body.currentPage;
         manage_data.method = "GET";
         manage_data.app_id = headers.app_id;
         manage_data.authorization = headers.authorization;
         return manage_data;
-    } else if(store == 'shopify') {
-        manage_data.endUrl = url_path + "/products.json?limit="+ body.pageSize+"&page="+ body.currentPage+"&collection_id="+ body.category_id;
-        manage_data.method = "GET";
+    } else if (store == 'shopify') {
+        let sort = false;
+        if(body.sortOrder.toUpperCase() == 'DESC'){
+            sort = true;
+        }
+        manage_data.body = `{
+            node(id: "${body.category_id}") {
+              id
+              ... on Collection {
+                    products(first: ${body.pageSize}, sortKey: ${body.sortBy.toUpperCase()}, reverse: ${sort}) {
+                            edges {
+                              node {
+                                id
+                                title
+                                handle
+                                description
+                                availableForSale
+                                createdAt
+                                descriptionHtml
+                                onlineStoreUrl
+                                productType
+                                publishedAt
+                                tags
+                                updatedAt
+                                images(first: 100) {
+                                        edges {
+                                          node {
+                                            id
+                                            originalSrc
+                                            transformedSrc
+                                            altText
+                                          }
+                                        }
+                                    }
+                                variants(first: 100) {
+                                    edges {
+                                      node {
+                                        id
+                                        sku
+                                        title
+                                        price
+                                        selectedOptions {
+                                          name
+                                          value
+                                        }
+                                      }
+                                    }
+                                  }
+                              }
+                              cursor
+                            }
+                            pageInfo {
+                              hasNextPage
+                            }
+                        }
+                    }
+                }
+            }
+          `;
+        manage_data.endUrl = url_path;
+        manage_data.method = "POST";
+        manage_data.contentType = "application/graphql";
+        manage_data.storefrontAccessToken = config.storefrontAccessToken;
         return manage_data;
     } else {
         //coming soon
@@ -37,3 +118,46 @@ export default {
     setPathForGetCategoryProduct
 };
 
+/*
+
+            node(id: "${body.category_id}") {
+              id
+              ... on Collection {
+                    products(first: ${body.pageSize}) {
+                            edges {
+                              node {
+                                id
+                                description
+                                availableForSale
+                                createdAt
+                                descriptionHtml
+                                onlineStoreUrl
+                                productType
+                                publishedAt
+                                tags
+                                updatedAt
+                                variants(first: 100) {
+                                  edges {
+                                    node {
+                                      id
+                                      sku
+                                      price
+                                      selectedOptions {
+                                        name
+                                        value
+                                      }
+                                    }
+                                  }
+                                }
+                              }
+                              cursor
+                            }
+                            pageInfo {
+                              hasNextPage
+                            }
+                        }
+                    }
+                }
+            }
+          `;
+          */
